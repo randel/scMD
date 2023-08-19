371fb9f88f9336a76378cfc0c8f3e6f40e691e4d
manqicai <32715449+manqicai@users.noreply.github.com>
2023-08-18 20:56:05 -0400

    Update scMD.R

--- R/scMD.R
+++ R/scMD.R
@@ -2,29 +2,25 @@
 ###################################################
 #' @title scMD framework
 #'
-#' @description  This function is used to perform signature generation and ensemble deconvolution using the scMD algorithm.
-#'
-#' @param bulk A matrix of bulk data.
-#' @param nc An integer specifying the number of cores for parallel computation. Default is 5.
-#' @param bulk_type A character specifying the technical platform of bulk data. Default is "450k". It can also be chosen from "450k", "850k", "WGBS", or "NULL".
-#' When set to NULL, the users should provide `dat` and `DM_df`.
-#' @param use_sc A character specifying the inbuilt scDNAm to be used. Default is "Luo". It can also be chosen from "Luo", "Tian", or "NULL".
-#' When set to NULL, the users should provide `dat` and `DM_df`.
-#' @param dat A beta matrix containing the scDNAm data. Default is NULL. When set to NULL, scMD will used inbuilt scDNAm data instead.
-#' @param DM_df A data frame containing information about differentially methylated CpG sites from scDNAm data.
-#' It should include columns for TargetID and celltype_ind. The columns celltype_ind contain p-values for each cell type.
-#' Default is NULL. When set to NULL, scMD will used inbuilt scDNAm data instead or the provided `dat` is already preprocessed.
-#' @param dmet_list A character vector specifying the list of deconvolution methods. Default is c("CIBERSORT", "EPIC", "FARDEEP", "DCQ",
-#'                 "DeconRNASeq", "BayesPrism", "ICeDT", "dtangle", "hspe").
-#' @param include_minfi A logical value indicating whether to include Minfi-based deconvolution. Default is TRUE.
-#' @param nmrk Number of markers per cell type used in deconvolution. Default is 100.
-#' @param celltype_ind A character vector specifying the cell types to be included in the scMD analysis. Default is NULL, which
-#'                     includes all cell types from the input data.
-#' @param includeMval A logical value indicating whether to include M values (beta values transformed to M values) for deconvolution.
-#'                    Default is TRUE.
-#' @param gen_sig A logical value indicating whether to generate signatures for deconvolution. Default is TRUE. When set to FALSE, it means
-#'                the input data is already preprocessed to only include markers for deconvolution.
-#' @param output_path A character specifying the path to save the output results. Default is the current directory (".").
+#' @description This function performs signature generation and ensemble deconvolution using the scMD algorithm.
+#' @param bulk A matrix of bulk data, with CpGs by sample.
+#' @param bulk_type A character string indicating the technical platform for the bulk data. Available options include "450k_or_850k", "WGBS", or "NULL".
+#' When set to "NULL", users are required to provide both `sc_mtx` and `DM_df`.
+#' @param use_sc A character specifying the inbuilt scDNAm to be used. Default is "Both". Options are "Luo", "Tian", "Both", or "NULL".
+#' When set to NULL, users must provide `sc_mtx` and `DM_df`.
+#' @param ncluster An integer specifying the number of cores for parallel computation. Default is 5.
+#' @param dmet_list A character vector specifying the desired deconvolution methods to be used.
+#' If NULL (the default), the following methods will be included:
+#' c("CIBERSORT", "EPIC", "FARDEEP", "DCQ", "ICeDT", "Houseman", "NNLS", "RPC").
+#' @param nmrk The number of markers per cell type used in deconvolution. Default is 100.
+#' @param gen_sig A logical value indicating whether to generate signatures for deconvolution. Default is TRUE. When FALSE,
+#' input data should already be preprocessed to only include markers for deconvolution.
+#' @param celltype_ind A character vector specifying the cell types included in the scMD analysis. Default is NULL, which includes all cell types from the input data.
+#' @param output_path A character specifying the path for saving intermediate results. Default is the current directory (".").
+#' @param sc_mtx A beta matrix containing the scDNAm data. Default is NULL. When NULL, scMD uses the inbuilt scDNAm data.
+#' @param DM_df A data frame with information about differentially methylated CpG sites from scDNAm data, including columns for TargetID and celltype_ind.
+#' Columns for celltype_ind should contain p-values for each cell type. Default is NULL.
+#' When NULL, scMD uses the inbuilt scDNAm data, or the provided `sc_mtx` is considered preprocessed.
 #'
 #' @return If scMD converges, return:
 #' \itemize{
@@ -37,48 +33,44 @@
 #' # Assuming 'my_bulk' and 'my_bulk_type' are defined
 #' scMD(bulk = my_bulk, bulk_type = my_bulk_type)
 #'
+#' @importFrom EpiDISH epidish
+#'
 #' @export
-scMD <- function(bulk,nc =5, bulk_type = "450k",use_sc = "Luo",dat = NULL,DM_df = NULL,
-                 dmet_list = c("CIBERSORT","EPIC","FARDEEP","DCQ","DeconRNASeq","BayesPrism","ICeDT","dtangle","hspe"),
-                 include_minfi = TRUE,nmrk = 100,
-                 celltype_ind =  NULL,
-                 includeMval = T, gen_sig = T,
-                 output_path = "."){
-  if(bulk_type == "450k"){
+scMD <- function(bulk, bulk_type = "450k_or_850k",use_sc = "Both",ncluster =5,dmet_list = NULL,
+                 nmrk = 100,gen_sig = T,celltype_ind =  NULL,output_path = ".",sc_mtx = NULL,DM_df = NULL,
+                  ){
+  if(bulk_type == "450k_or_850k"){
     load
   }
-  overlap_features = intersect(rownames(dat),rownames(bulk))
-  dat <- dat[overlap_features,]
+  overlap_features = intersect(rownames(sc_mtx),rownames(bulk))
+  sc_mtx <- sc_mtx[overlap_features,]
   bulk <- bulk[overlap_features,]
 
 
   if(is.null(celltype_ind)){
-    celltype_ind = colnames(dat)
+    celltype_ind = colnames(sc_mtx)
   }
 
   if(gen_sig){
-    sig <- get_sig(nmrk = nmrk,dat,DM_df,ct_ind = celltype_ind)
+    sig <- get_sig(nmrk = nmrk,sc_mtx,DM_df,ct_ind = celltype_ind)
   }else{
-    sig <- dat
+    sig <- sc_mtx
   }
 
   bulk <- bulk[intersect(rownames(sig),rownames(bulk)),]
 
-  if(include_minfi){
-    frac_est <-  est_frac(sig, bulk)
+
+  frac_est <-  est_frac(sig, bulk)
 
 
-    frac_rpc <-  EpiDISH::epidish(bulk, sig,method = 'RPC')$estF
+  frac_rpc <-  EpiDISH::epidish(bulk, sig,method = 'RPC')$estF
 
-    minfi_res <- DNAm_minfi_deconv_general(dat = sig,metaref = data.frame(SamplesName = colnames(sig), deconv_clust = colnames(sig)),
-                                           bulk =bulk,true_frac = NULL,findMrks = F)
-    if(includeMval){
-      phat_all <- list(NNLS = frac_est, Houseman_beta = minfi_res[[2]]$counts, Houseman_m = minfi_res[[1]]$counts,RPC = frac_rpc)
-    }else{
-      phat_all <- list(NNLS = frac_est, Houseman_beta = minfi_res[[2]]$counts,RPC = frac_rpc)
-    }
-    saveRDS(phat_all, file = paste0(output_path,"phat_all.rds"))
-  }
+  minfi_res <- DNAm_minfi_deconv_general(dat = sig,metaref = data.frame(SamplesName = colnames(sig), deconv_clust = colnames(sig)),
+                                         bulk =bulk,true_frac = NULL,findMrks = F)
+  phat_all <- list(NNLS = frac_est, Houseman_beta = minfi_res[[2]]$counts, Houseman_m = minfi_res[[1]]$counts,RPC = frac_rpc)
+
+  saveRDS(phat_all, file = paste0(output_path,"phat_all.rds"))
+
 
 
     ref_list = list(beta = list(ref_matrix = sig, meta_ref = data.frame(SamplesName = colnames(sig), deconv_clust = colnames(sig)),
@@ -90,34 +82,28 @@ scMD <- function(bulk,nc =5, bulk_type = "450k",use_sc = "Luo",dat = NULL,DM_df 
                                                               n_markers = 50, Marker.Method = "none",
                                                               TNormalization = c("none"), CNormalization = c("none"),
                                                               Scale = c("linear","log"), dmeths = dmet_list),
-                                          outpath = paste0(output_path,"beta"),parallel_comp = T,ncore = nc))
+                                          outpath = paste0(output_path,"beta"),parallel_comp = T,ncore = ncluster))
 
 
 
     phat_list1 <- unlist(lapply(res_Ens$allgene_res, getphat),recursive = FALSE)
 
-    if(includeMval){
-      ref_list = list(Mval = list(ref_matrix = BetaToMvalue(sig), meta_ref = data.frame(SamplesName = colnames(sig), deconv_clust = colnames(sig)),
-                                  data_name = "Mval"))
+    ref_list = list(Mval = list(ref_matrix = BetaToMvalue(sig), meta_ref = data.frame(SamplesName = colnames(sig), deconv_clust = colnames(sig)),
+                                data_name = "Mval"))
 
-      suppressWarnings(res_Ens_mval <- EnsDeconv(count_bulk = BetaToMvalue(bulk),
-                                                 ref_list  = ref_list,
-                                                 params = get_params(data_type = "singlecell-rna", data_name = "Mval",
-                                                                     n_markers = 50, Marker.Method = "none",
-                                                                     TNormalization = c("none"), CNormalization = c("none"),
-                                                                     Scale = c("linear"), dmeths = dmet_list),
-                                                 outpath = output_path,parallel_comp = T,ncore = nc))
+    suppressWarnings(res_Ens_mval <- EnsDeconv(count_bulk = BetaToMvalue(bulk),
+                                               ref_list  = ref_list,
+                                               params = get_params(data_type = "singlecell-rna", data_name = "Mval",
+                                                                   n_markers = 50, Marker.Method = "none",
+                                                                   TNormalization = c("none"), CNormalization = c("none"),
+                                                                   Scale = c("linear"), dmeths = dmet_list),
+                                               outpath = output_path,parallel_comp = T,ncore = ncluster))
 
-      phat_list2 <- unlist(lapply(res_Ens_mval$allgene_res, getphat),recursive = FALSE)
-      phat_list1 <- append(phat_list1, phat_list2)
-    }
+    phat_list2 <- unlist(lapply(res_Ens_mval$allgene_res, getphat),recursive = FALSE)
+    phat_list1 <- append(phat_list1, phat_list2)
 
+    phat_all <- append(phat_all, phat_list1)
 
-    if(include_minfi){
-      phat_all <- append(phat_all, phat_list1)
-    }else{
-      phat_all <- phat_list1
-    }
 
 
   saveRDS(phat_all, file = paste0(output_path,"phat_all.rds"))
